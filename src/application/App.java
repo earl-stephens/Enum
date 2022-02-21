80e217b5547566149c835643038d2cfe7e838ddc
earl-stephens <stephensearl@hotmail.com>
2022-02-20 23:20:41 -0700

    Create a simple enum

--- src/application/App.java
+++ src/application/App.java
@@ -3,8 +3,19 @@ package application;
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		/* Enum is a top level type in Java
+		 * Designed for when you have a list of almost
+		 * identical things, like colors
+		 */
+		Fruit fruit1 = Fruit.APPLE;
+		Fruit fruit2 = Fruit.ORANGE;
+		
+		System.out.println(fruit1 == fruit2);
+		
+		Fruit fruit3 = Fruit.BANANA;
+		Fruit fruit4 = Fruit.BANANA;
+		
+		System.out.println(fruit3 == fruit4);
 	}
 
 }
